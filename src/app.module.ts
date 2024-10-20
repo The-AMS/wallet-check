cf2c551047c1422f013137f0c4086999c1db3a12
The_AMS <moeinati.79@gmail.com>
2024-10-20 21:39:26 +0330

    file structures

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TelegramModule } from './telegram/telegram.module';
+import { WalletModule } from './wallet/wallet.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [TelegramModule, WalletModule, TransactionModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
