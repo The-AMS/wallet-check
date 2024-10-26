f2f07c6da66883056fc58dac49af25e79b6ae94a
The-AMS <moeinati.79@gmail.com>
2024-10-26 08:20:07 +0330

    Database Connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { TelegramModule } from './telegram/telegram.module';
 import { DatabaseModule } from './database/database.module';
 import { BscScanModule } from './bsc-scan/bsc-scan.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     TelegramModule,
     DatabaseModule,
     BscScanModule,
--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import * as TelegramBot from 'node-telegram-bot-api';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class TelegramService implements OnModuleInit {
@@ -10,11 +11,16 @@ export class TelegramService implements OnModuleInit {
     '/setwallet - Sets a BSC wallet to observe.',
     '/checktransaction - Fetches the last N transactions for the set wallet.',
   ];
+
+  // injecting the data base service
+  constructor(private readonly databaseService: DatabaseService) {}
+
   onModuleInit() {
     const token = process.env.TELEGRAM_BOT_TOKEN;
     this.bot = new TelegramBot(token, { polling: true });
     this.regirterCommands();
   }
+
   private regirterCommands() {
     // start
     this.bot.onText(/\/start/, (msg) => {
@@ -24,6 +30,7 @@ export class TelegramService implements OnModuleInit {
         'Welcome to our bot!\nTo learn more, please enter the /help command.',
       );
     });
+
     // help
     this.bot.onText(/\/help/, (msg) => {
       const chatId = msg.chat.id;
@@ -31,6 +38,25 @@ export class TelegramService implements OnModuleInit {
         chatId,
         `Here is available command list: \n${this.commandList.join('\n')}`,
       );
+    });
+
+    // setwallet
+    this.bot.onText(/\/setwallet (.+)/, async (msg, match) => {
+      const chatId = msg.chat.id;
+      const walletAddress = match[1];
+
+      try {
+        await this.databaseService.saveUser(chatId, walletAddress);
+        this.bot.sendMessage(
+          chatId,
+          `Wallet address ${walletAddress} saved successfully.`,
+        );
+      } catch (error) {
+        this.bot.sendMessage(
+          chatId,
+          `An error occurred while saving your wallet address.\nerror :${error}`,
+        );
+      }
     });
   }
 }
